b37060387ba3851d8683079ea3f8d488f7bc1fe3
jae9380 <ljy5314@gmail.com>
2024-02-20 14:07:06 +0900

    create subnet

---
+++ project1/variables.tf
@@ -0,0 +1,14 @@
+variable "prefix" { // prefix 변수 이름
+  description = "Prefix for all resources"
+  default     = "dev" // prefix 변수의 값은 dev
+}
+
+variable "region" {
+  description = "region"
+  default     = "ap-northeast-2"
+}
+
+variable "nickname" {
+  description = "nickname"
+  default     = "jhs512"
+}
